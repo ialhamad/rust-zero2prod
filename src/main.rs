6e53ab1f2f965526db209d364b9c2686b054df7a
Ibrahim Alhamad <i.alhamad@5fsoftware.de>
2021-10-26 22:24:01 +0200

    Finished the telemtery chapter

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use std::net::TcpListener;
 
 use sqlx::PgPool;
-use zero2prod::{configuration::get_configuration, startup::run};
+use zero2prod::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Panic if we can't read configuration
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
+
     let db_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
-    // We have removed the hard-coded `8000`- it's now coming from our settings!
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, db_pool)?.await
